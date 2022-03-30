d72e4afbcb8358317b4044f94504bc55ef31e0f6
DarkWool <cooper27@live.com.mx>
2022-03-29 18:17:57 -0600

    Added new route for info about the anxiety test, styles and minor improvements.

--- anxiety_app/views.py
+++ anxiety_app/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/test-info")
+def info():
+    return render_template("info.html")
