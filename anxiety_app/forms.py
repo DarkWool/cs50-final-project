b10df4826df48eee772336158706f43cd92aeb38
DarkWool <cooper27@live.com.mx>
2022-04-16 20:37:32 -0500

    Modified form messages and validators.

--- anxiety_app/forms.py
+++ anxiety_app/forms.py
@@ -3,12 +3,14 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class SignUpForm(FlaskForm):
     username = StringField("Username", [
-        validators.InputRequired(), 
-        validators.Length(min=4, max=20, message="Username must be at least 4 characters long and max 20")
+        validators.InputRequired(),
+        validators.DataRequired(), 
+        validators.Length(min=2, max=20, message="Username must be at least 2 characters long and a max of 20.")
     ])
 
     firstName = StringField("First Name", [
         validators.InputRequired(),
+        validators.DataRequired(), 
         validators.Length(min=2, max=20),
     ])
 
@@ -21,8 +23,8 @@ class SignUpForm(FlaskForm):
         validators.InputRequired(),
         validators.Regexp("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,24}$", 0,
             '''Password must contain at least one lowercase and one capital letter, a number and it must be 
-            between 8 and 24 characters long'''),
-        validators.EqualTo("confirmPassword", message="Passwords doesn't match")
+            between 8 and 24 characters long with no spaces.'''),
+        validators.EqualTo("confirmPassword", message="Passwords don't match.")
     ])
 
     confirmPassword = PasswordField("Repeat Password", [
@@ -45,14 +47,12 @@ class changePasswordForm(FlaskForm):
 
     newPassword = PasswordField("New password", [
         validators.InputRequired(),
-        validators.DataRequired(),
         validators.Regexp("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,24}$", 0,
             '''Password must contain at least one lowercase and one capital letter, a number and it must be 
-            between 8 and 24 characters long'''),
-        validators.EqualTo("confirmPassword", message="Passwords doesn't match")
+            between 8 and 24 characters long with no spaces.'''),
+        validators.EqualTo("confirmPassword", message="Passwords don't match.")
     ])
 
     confirmPassword = PasswordField("Confirm password", [
-        validators.InputRequired(),
-        validators.DataRequired()
+        validators.InputRequired()
     ])
